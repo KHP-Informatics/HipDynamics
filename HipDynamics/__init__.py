ffc04ca1299e54a55290412fa08d9d35b8d178d4
Maximilian Kerz <kerz.maximilian@gmail.com>
2017-01-27 18:56:54 +0000

    Package __init__.py fix.

--- HipDynamics/__init__.py
+++ HipDynamics/__init__.py
@@ -1,4 +1,4 @@
 from HipDynamics.analysis import Analysis, AnalysisWrapper
 from HipDynamics.staging import LookUpTable, LookUpTableWrapper, TableSetup
 from HipDynamics.vis import QuickPlot
-
+__all__ = ['Analysis', 'AnalysisWrapper', 'LookUpTable', 'LookUpTableWrapper', 'TableSetup', 'QuickPlot']
